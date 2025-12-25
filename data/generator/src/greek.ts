6fd04c2a3fcd939bce80468f6f3d97ee2820ec86
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-12-26 00:24:54 +0530

    Enhance Greek Character Definitions
    Introduce a structured definition for each Greek character, including a `category` and a list of `keywords`. This facilitates easier querying and filtering based on character type and common usage

--- data/generator/src/greek.ts
+++ data/generator/src/greek.ts
@@ -1,21 +1,162 @@
 export const greek = {
-    "α": ["alpha", "greek"],
-    "β": ["beta", "greek"],
-    "γ": ["gamma", "greek"],
-    "δ": ["delta", "greek"],
-    "ε": ["epsilon", "greek"],
-    "ζ": ["zeta", "greek"],
-    "η": ["eta", "greek"],
-    "θ": ["theta", "greek"],
-    "λ": ["lambda", "greek"],
-    "μ": ["mu", "micro", "greek"],
-    "π": ["pi", "greek"],
-    "ρ": ["rho", "greek"],
-    "σ": ["sigma", "greek"],
-    "τ": ["tau", "greek"],
-    "φ": ["phi", "greek"],
-    "χ": ["chi", "greek"],
-    "ψ": ["psi", "greek"],
-    "ω": ["omega", "greek"],
-    "Ω": ["omega", "ohm", "greek", "capital"]
+    α: {
+        category: "greek",
+        keywords: ["alpha", "a"],
+    },
+    β: {
+        category: "greek",
+        keywords: ["beta", "b"],
+    },
+    γ: {
+        category: "greek",
+        keywords: ["gamma", "g"]
+    },
+    δ: {
+        category: "greek",
+        keywords: ["delta", "d"]
+    },
+    ε: {
+        category: "greek",
+        keywords: ["epsilon", "e"]
+    },
+    ζ: {
+        category: "greek",
+        keywords: ["zeta", "z"]
+    },
+    η: {
+        category: "greek",
+        keywords: ["eta", "h"]
+    },
+    θ: {
+        category: "greek",
+        keywords: ["theta", "t"]
+    },
+    λ: {
+        category: "greek",
+        keywords: ["lambda", "l"]
+    },
+    μ: {
+        category: "greek",
+        keywords: ["mu", "m"]
+    },
+    π: {
+        category: "greek",
+        keywords: ["pi", "p"]
+    },
+    ρ: {
+        category: "greek",
+        keywords: ["rho", "r"]
+    },
+    σ: {
+        category: "greek",
+        keywords: ["sigma", "s"]
+    },
+    τ: {
+        category: "greek",
+        keywords: ["tau", "u"]
+    },
+    φ: {
+        category: "greek",
+        keywords: ["phi", "f"]
+    },
+    χ: {
+        category: "greek",
+        keywords: ["chi", "x"]
+    },
+    ψ: {
+        category: "greek",
+        keywords: ["psi", "y"]
+    },
+    ω: {
+        category: "greek",
+        keywords: ["omega", "o"]
+    },
+    Α: {
+        category: "greek",
+        keywords: ["alpha", "capital"]
+    },
+    Β: {
+        category: "greek",
+        keywords: ["beta", "capital"]
+    },
+    Γ: {
+        category: "greek",
+        keywords: ["gamma", "capital"]
+    },
+    Δ: {
+        category: "greek",
+        keywords: ["delta", "capital"]
+    },
+    Ε: {
+        category: "greek",
+        keywords: ["epsilon", "capital"]
+    },
+    Ζ: {
+        category: "greek",
+        keywords: ["zeta", "capital"]
+    },
+    Η: {
+        category: "greek",
+        keywords: ["eta", "capital"]
+    },
+    Θ: {
+        category: "greek",
+        keywords: ["theta", "capital"]
+    },
+    Λ: {
+        category: "greek",
+        keywords: ["lambda", "capital"]
+    },
+    Μ: {
+        category: "greek",
+        keywords: ["mu", "capital"]
+    },
+    Ν: {
+        category: "greek",
+        keywords: ["nu", "capital"]
+    },
+    Ξ: {
+        category: "greek",
+        keywords: ["xi", "capital"]
+    },
+    Ο: {
+        category: "greek",
+        keywords: ["omicron", "capital"]
+    },
+    Π: {
+        category: "greek",
+        keywords: ["pi", "capital"]
+    },
+    Ρ: {
+        category: "greek",
+        keywords: ["rho", "capital"]
+    },
+    Σ: {
+        category: "greek",
+        keywords: ["sigma", "capital"]
+    },
+    Τ: {
+        category: "greek",
+        keywords: ["tau", "capital"]
+    },
+    Υ: {
+        category: "greek",
+        keywords: ["upsilon", "capital"]
+    },
+    Φ: {
+        category: "greek",
+        keywords: ["phi", "capital"]
+    },
+    Χ: {
+        category: "greek",
+        keywords: ["chi", "capital"]
+    },
+    Ψ: {
+        category: "greek",
+        keywords: ["psi", "capital"]
+    },
+    Ω: {
+        category: "greek",
+        keywords: ["omega", "capital"]
+    }
 }
