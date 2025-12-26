316cf8704fcc3f825fae1b422f68bf49896c3436
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-12-26 14:16:35 +0530

    Fetch and render character map
    - Adds a search input field to `index.html`
    - Fetches character map data from `/charmap.json` using `fetch`
    - Parses JSON data into `CHARMAP`
    - Dynamically render the character map into a section with categorized symbols on `index.html`
    - Handles potential fetch and JSON parsing errors gracefully

--- web/script.js
+++ web/script.js
@@ -1,1 +1,76 @@
-console.log("Hello Character Map!!!")
+/** The full character map */
+let CHARMAP = {}
+
+async function loadCharacterMap() {
+
+    let response
+
+    try {
+        response = await fetch('/charmap.json')
+    } catch (e) {
+        console.error(`Failed to fetch character map: ${e}`)
+    }
+
+    if (!response) {
+        console.error('Failed to fetch character map')
+        return
+    }
+
+    if (!response.ok) {
+        console.error(`Failed to fetch character map: ${response.status}`)
+        return
+    }
+
+    try {
+        CHARMAP = await response.json()
+    } catch (e) {
+        console.error(`Failed to parse character map json: ${e}`)
+    }
+
+    console.info('Loaded character map')
+
+    renderCharacterMap()
+}
+
+function renderCharacterMap() {
+    const charMapSection = /** @type HTMLSection */ (document.getElementById('character-map'))
+    if (!charMapSection) {
+        console.error('Failed to find character map section')
+        return
+    }
+
+    const symbolsGroupedByCategory = Object.groupBy(Object.values(CHARMAP), c => c.category)
+
+    for (const [category, symbols] of Object.entries(symbolsGroupedByCategory)) {
+        renderCharacterSection(charMapSection, category, symbols);
+    }
+}
+
+function renderCharacterSection(parent, category, symbols) {
+    const section = document.createElement('section');
+    section.id = category;
+    section.classList.add('category');
+
+    const header = document.createElement('h2');
+    header.textContent = category;
+    section.appendChild(header);
+
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    section.appendChild(grid);
+
+    for (const symbol of symbols) {
+        renderSymbol(grid, symbol);
+    }
+
+    parent.appendChild(section);
+}
+
+function renderSymbol(parent, symbol) {
+    const div = document.createElement('div')
+    div.classList.add('symbol')
+    div.innerHTML = symbol.symbol
+    parent.appendChild(div)
+}
+
+document.addEventListener('DOMContentLoaded', loadCharacterMap)
