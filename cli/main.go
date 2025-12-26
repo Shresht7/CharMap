89b3b1e0892467e3beab8cb17b2298da214567f7
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-12-26 21:06:21 +0530

    Initialize bubbletea application

--- cli/main.go
+++ cli/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	list []string
+}
+
+func (m *model) Init() tea.Cmd {
+	return nil
+}
+
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		default:
+			return m, nil
+		}
+	}
+	return m, nil
+}
+
+func (m *model) View() string {
+	var s strings.Builder
+	for _, r := range m.list {
+		s.WriteString(fmt.Sprintf("%s\n", r))
+	}
+	return s.String()
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	initialModel := &model{
+		list: []string{"Item 1", "Item 2", "Item 3"},
+	}
+	p := tea.NewProgram(initialModel)
+	if _, err := p.Run(); err != nil {
+		fmt.Println(err)
+	}
 }
