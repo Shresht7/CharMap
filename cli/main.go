16cca4d1a547953db00c38b01582aeb5025d6640
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-12-26 21:08:15 +0530

    Move bubbletea app logic into `app.go`

--- cli/main.go
+++ cli/main.go
@@ -2,39 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
-
-type model struct {
-	list []string
-}
-
-func (m *model) Init() tea.Cmd {
-	return nil
-}
-
-func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "ctrl+c":
-			return m, tea.Quit
-		default:
-			return m, nil
-		}
-	}
-	return m, nil
-}
-
-func (m *model) View() string {
-	var s strings.Builder
-	for _, r := range m.list {
-		s.WriteString(fmt.Sprintf("%s\n", r))
-	}
-	return s.String()
-}
 
 func main() {
 	initialModel := &model{
