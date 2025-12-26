fd32ca5ebc5ae7eda8b1c1ed60736c2216727bb3
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-12-26 21:51:48 +0530

    Implement Symbol Definition and Loading from JSON file
    This commit introduces functionality to load Unicode symbols from a JSON file. The `LoadSymbols` function opens the file, decodes the JSON data, and returns a slice of `Symbol` structs

---
+++ cli/chars.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Symbol struct {
+	Symbol      string   `json:"symbol"`
+	Category    string   `json:"category"`
+	Unicode     string   `json:"unicode"`
+	Decimal     int      `json:"decimal"`
+	Latex       string   `json:"latex"`
+	Keywords    []string `json:"keywords"`
+	Description string   `json:"description"`
+}
+
+func LoadSymbols(path string) ([]Symbol, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var symbols []Symbol
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&symbols); err != nil {
+		return nil, err
+	}
+
+	return symbols, nil
+}
